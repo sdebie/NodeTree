b40524f1fdae2f33261f54ac94436be6c6b753ee
Shawn de Bie <shawn.debie@mobile-data.co.za>
2016-09-20 15:39:05 +0200

    Added demo code

--- nodetreeinfo/src/main/java/org/nodetreeinfo/NodeTree.java
+++ nodetreeinfo/src/main/java/org/nodetreeinfo/NodeTree.java
@@ -5,10 +5,70 @@
  */
 package org.nodetreeinfo;
 
+import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.view.mxGraph;
+import javax.swing.JFrame;
+import org.jgraph.graph.DefaultEdge;
+import org.jgrapht.DirectedGraph;
+import org.jgrapht.ListenableGraph;
+import org.jgrapht.graph.DefaultListenableGraph;
+import org.jgrapht.graph.DirectedMultigraph;
+
 /**
  *
  * @author shawndebie
  */
-public class NodeTree {
+public class NodeTree extends JFrame {
 
-}
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = -2707712944901661771L;
+
+	public NodeTree()
+	{
+		super("Hello, World!");
+
+		mxGraph graph = new mxGraph();
+		Object parent = graph.getDefaultParent();
+
+		graph.getModel().beginUpdate();
+		try {
+			Object v1 = graph.insertVertex(parent, null, "Hello", 20, 20, 80,
+					30);
+			Object v2 = graph.insertVertex(parent, null, "World!", 240, 150,
+					80, 30);
+			graph.insertEdge(parent, null, "Edge", v1, v2);
+		} finally {
+			graph.getModel().endUpdate();
+		}
+
+		mxGraphComponent graphComponent = new mxGraphComponent(graph);
+		getContentPane().add(graphComponent);
+	}
+
+	public static void main(String[] args)
+	{
+		NodeTree frame = new NodeTree();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 320);
+		frame.setVisible(true);
+	}
+
+	public static void InitTree2()
+	{
+		ListenableGraph<String, DefaultEdge> g = new ListenableDirectedMultigraph<String, DefaultEdge>(DefaultEdge.class);
+	}
+
+	private static class ListenableDirectedMultigraph<V, E>
+			extends DefaultListenableGraph<V, E>
+			implements DirectedGraph<V, E> {
+
+		private static final long serialVersionUID = 1L;
+
+		ListenableDirectedMultigraph(Class<E> edgeClass)
+		{
+			super(new DirectedMultigraph<V, E>(edgeClass));
+		}
+	}
+}//NodeTree
