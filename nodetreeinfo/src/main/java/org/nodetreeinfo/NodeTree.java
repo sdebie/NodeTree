bac6f5193d16edf9427584c9d722097248a01e47
Shawn de Bie <shawn.debie@mobile-data.co.za>
2016-09-20 15:54:57 +0200

    Added demo code

--- nodetreeinfo/src/main/java/org/nodetreeinfo/NodeTree.java
+++ nodetreeinfo/src/main/java/org/nodetreeinfo/NodeTree.java
@@ -5,6 +5,8 @@
  */
 package org.nodetreeinfo;
 
+import com.mxgraph.layout.mxCompactTreeLayout;
+import com.mxgraph.model.mxGraphModel;
 import com.mxgraph.swing.mxGraphComponent;
 import com.mxgraph.view.mxGraph;
 import javax.swing.JFrame;
@@ -20,55 +22,86 @@ import org.jgrapht.graph.DirectedMultigraph;
  */
 public class NodeTree extends JFrame {
 
-	/**
-	 *
-	 */
 	private static final long serialVersionUID = -2707712944901661771L;
+	private static mxGraph graph;
+
+//	private static Database db;
+	private static String MAINDB_USER = "vodacom";
+	private static String MAINDB_PASS = "vodacom";
 
 	public NodeTree()
 	{
-		super("Hello, World!");
 
-		mxGraph graph = new mxGraph();
+	}
+
+	/**
+	 *
+	 */
+	public static void InitTree()
+	{
+
+		graph = new mxGraph();
+		mxCompactTreeLayout vLayout = new mxCompactTreeLayout(graph);
 		Object parent = graph.getDefaultParent();
 
 		graph.getModel().beginUpdate();
 		try {
-			Object v1 = graph.insertVertex(parent, null, "Hello", 20, 20, 80,
-					30);
-			Object v2 = graph.insertVertex(parent, null, "World!", 240, 150,
-					80, 30);
-			graph.insertEdge(parent, null, "Edge", v1, v2);
+			graph.insertVertex(parent, "1", "Kick", 1, 1, 80, 30);
+			graph.insertVertex(parent, "2", "Ass", 1, 1, 80, 30);
+
+			mxGraphModel test = new mxGraphModel();
+			Object cell1 = test.getChildAt(parent, 0);
+
+			Object cell2 = test.getChildAt(parent, 1);
+			graph.insertEdge(parent, "", "", cell1, cell2);
+
+			vLayout.setHorizontal(false);
+			vLayout.setLevelDistance(10);
+			vLayout.setNodeDistance(20);
+			vLayout.setResetEdges(true);
+			vLayout.setEdgeRouting(true);
+
+			vLayout.execute(parent);
+
 		} finally {
 			graph.getModel().endUpdate();
 		}
 
-		mxGraphComponent graphComponent = new mxGraphComponent(graph);
-		getContentPane().add(graphComponent);
-	}
+	}//InitTree
 
 	public static void main(String[] args)
 	{
-		NodeTree frame = new NodeTree();
+		InitTree();
+		JFrame frame = new JFrame();
+		mxGraphComponent graphComponent = new mxGraphComponent(graph);
+		frame.getContentPane().add(graphComponent);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(400, 320);
 		frame.setVisible(true);
 	}
 
-	public static void InitTree2()
+	//----------------------------------------------------------[ getTestDB ]---
+	public static String getTestDB()
 	{
-		ListenableGraph<String, DefaultEdge> g = new ListenableDirectedMultigraph<String, DefaultEdge>(DefaultEdge.class);
-	}
+//		return "jdbc:oracle:thin:" + MAINDB_USER + "/" + MAINDB_PASS + "@proddb:1521:proddb";
+		return "jdbc:oracle:thin:" + MAINDB_USER + "/" + MAINDB_PASS + "@oradev:1521:oradev";
+	}//getOLTPTestDB
 
-	private static class ListenableDirectedMultigraph<V, E>
-			extends DefaultListenableGraph<V, E>
-			implements DirectedGraph<V, E> {
+	//------------------------------------------------------[ switchContext ]---
+	public static void switchContext(int pEnv) throws Exception
+	{
+		String vDatabaseUrl;
 
-		private static final long serialVersionUID = 1L;
+		vDatabaseUrl = getTestDB();
 
-		ListenableDirectedMultigraph(Class<E> edgeClass)
-		{
-			super(new DirectedMultigraph<V, E>(edgeClass));
-		}
-	}
+		try {
+//			db = Database.getInstance(vDatabaseUrl);
+		}//if
+		catch (Exception Ex) {
+			//Try again just in case it now works :)
+//			db = Database.getInstance(vDatabaseUrl);
+		}//catch
+//		db.setContext("USER", "admin");
+//		db.setDefault();
+	}//switchContext
 }//NodeTree
