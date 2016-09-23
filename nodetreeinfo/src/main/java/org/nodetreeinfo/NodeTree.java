d55ef15ad56fb2073ba69c17795364702d13985f
Shawn de Bie <shawn.debie@mobile-data.co.za>
2016-09-23 11:44:13 +0200

    Working

--- nodetreeinfo/src/main/java/org/nodetreeinfo/NodeTree.java
+++ nodetreeinfo/src/main/java/org/nodetreeinfo/NodeTree.java
@@ -5,17 +5,32 @@
  */
 package org.nodetreeinfo;
 
+import com.mxgraph.layout.hierarchical.mxHierarchicalLayout;
 import com.mxgraph.layout.mxCompactTreeLayout;
 import com.mxgraph.model.mxCell;
 import com.mxgraph.model.mxGraphModel;
+import com.mxgraph.swing.handler.mxRubberband;
 import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.util.mxConstants;
 import com.mxgraph.view.mxGraph;
+import com.mxgraph.view.mxStylesheet;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import javax.swing.JFrame;
 import org.jgraph.graph.DefaultEdge;
 import org.jgrapht.DirectedGraph;
 import org.jgrapht.ListenableGraph;
 import org.jgrapht.graph.DefaultListenableGraph;
 import org.jgrapht.graph.DirectedMultigraph;
+import org.tradeswitch.base.DBException;
+import org.tradeswitch.base.Database;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
+import javafx.scene.control.Cell;
 
 /**
  *
@@ -25,65 +40,66 @@ public class NodeTree extends JFrame {
 
 	private static final long serialVersionUID = -2707712944901661771L;
 	private static mxGraph graph;
-
-//	private static Database db;
-	private static String MAINDB_USER = "vodacom";
-	private static String MAINDB_PASS = "vodacom";
+	private static Connection conn;
 
 	public NodeTree()
 	{
 
 	}
 
-	/**
-	 *
-	 */
-	public static void InitTree()
+	public static void InitTree() throws DBException, SQLException
 	{
 
 		graph = new mxGraph();
-		mxCompactTreeLayout vLayout = new mxCompactTreeLayout(graph);
+		mxHierarchicalLayout vLayout = new mxHierarchicalLayout(graph);
 		Object parent = graph.getDefaultParent();
+		mxGraphModel vModel = new mxGraphModel();
+		LinkedHashMap<Integer, Integer> relasions = new LinkedHashMap<>();
 
 		graph.getModel().beginUpdate();
 		try {
-			graph.insertVertex(parent, "1", "Kick", 1, 1, 80, 30);
-			graph.insertVertex(parent, "2", "Ass", 1, 1, 80, 30);
-			graph.insertVertex(parent, "3", "Node One", 1, 1, 80, 30);
-			graph.insertVertex(parent, "4", "Node 2", 1, 1, 80, 30);
-			graph.insertVertex(parent, "5", "Node 3", 1, 1, 80, 30);
+			int iIndex = 0;
+			Statement st = conn.createStatement();
+			ResultSet rs = st.executeQuery("select * from NODES order by NDS_IRN");
 
-			graph.insertVertex(parent, "6", "Level 3-One", 1, 1, 80, 30);
-			graph.insertVertex(parent, "7", "Leverl 3-2", 1, 1, 80, 30);
-			graph.insertVertex(parent, "8", "Leevl 3-3", 1, 1, 80, 30);
+			while (rs.next()) {
+				String vNodeName = rs.getString("NDS_NAME");
+				Long vNodeID = rs.getLong("NDS_IRN");
+				graph.insertVertex(parent, String.valueOf(vNodeID), vNodeName, 1, 1, 80, 30, "fillColor=green");
 
-			mxGraphModel test = new mxGraphModel();
-			int iIndex = 1;
-			while (iIndex < 5) {
-				Object cell1 = test.getChildAt(parent, 0);
-				mxCell cell2 = (mxCell) test.getChildAt(parent, iIndex);
-				if (cell2.getId().isEmpty()) {
-					break;
-				}//if
-				graph.insertEdge(parent, "", "", cell1, cell2);
+				//Add Parent and Index
+				relasions.put(vNodeID.intValue(), iIndex);
 				iIndex++;
 			}//while
 
-			while (true) {
-				Object cell1 = test.getChildAt(parent, 2);
-				mxCell cell2 = (mxCell) test.getChildAt(parent, iIndex);
-				if (cell2.getId().isEmpty()) {
-					break;
+			iIndex = 0;
+			Object vChild;
+			mxCell vParent;
+			rs = st.executeQuery("select * from NODES order by NDS_IRN");
+			while (rs.next()) {
+				if (rs.getInt("NDS_NDS_IRN") > 0) {
+					int vParentIndex = relasions.get(rs.getInt("NDS_NDS_IRN"));
+					vChild = vModel.getChildAt(parent, iIndex);
+					vParent = (mxCell) vModel.getChildAt(parent, vParentIndex);
+					graph.insertEdge(parent, "", "", vParent, vChild);
 				}//if
-				graph.insertEdge(parent, "", "", cell1, cell2);
 				iIndex++;
 			}//while
 
-			vLayout.setHorizontal(false);
-			vLayout.setLevelDistance(10);
-			vLayout.setNodeDistance(20);
-			vLayout.setResetEdges(true);
-			vLayout.setEdgeRouting(true);
+			vLayout.setResizeParent(false);
+			vLayout.setMoveParent(false);
+			vLayout.setParentBorder(0);
+
+			vLayout.setIntraCellSpacing(30);
+			vLayout.setInterRankCellSpacing(50);
+			vLayout.setInterHierarchySpacing(10);
+			vLayout.setParallelEdgeSpacing(10);
+
+			vLayout.setOrientation(mxConstants.DIRECTION_MASK_WEST);
+
+			vLayout.setFineTuning(true);
+			//vLayout.
+			vLayout.setDisableEdgeStyle(false);
 
 			vLayout.execute(parent);
 
@@ -93,39 +109,31 @@ public class NodeTree extends JFrame {
 
 	}//InitTree
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
+		conn = connectToDatabaseOrDie();
 		InitTree();
 		JFrame frame = new JFrame();
 		mxGraphComponent graphComponent = new mxGraphComponent(graph);
+		graphComponent.setEnabled(false);
 		frame.getContentPane().add(graphComponent);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(800, 600);
 		frame.setVisible(true);
 	}
 
-	//----------------------------------------------------------[ getTestDB ]---
-	public static String getTestDB()
+	private static Connection connectToDatabaseOrDie()
 	{
-//		return "jdbc:oracle:thin:" + MAINDB_USER + "/" + MAINDB_PASS + "@proddb:1521:proddb";
-		return "jdbc:oracle:thin:" + MAINDB_USER + "/" + MAINDB_PASS + "@oradev:1521:oradev";
-	}//getOLTPTestDB
-
-	//------------------------------------------------------[ switchContext ]---
-	public static void switchContext(int pEnv) throws Exception
-	{
-		String vDatabaseUrl;
-
-		vDatabaseUrl = getTestDB();
-
 		try {
-//			db = Database.getInstance(vDatabaseUrl);
-		}//if
-		catch (Exception Ex) {
-			//Try again just in case it now works :)
-//			db = Database.getInstance(vDatabaseUrl);
-		}//catch
-//		db.setContext("USER", "admin");
-//		db.setDefault();
-	}//switchContext
+			Class.forName("org.postgresql.Driver");
+			String url = "jdbc:postgresql://devpg:5432/qa?currentSchema=db";
+			conn = DriverManager.getConnection(url, "qa", "qa");
+		} catch (ClassNotFoundException e) {
+			System.exit(1);
+		} catch (SQLException e) {
+			System.exit(2);
+		}
+		return conn;
+	}
+
 }//NodeTree
